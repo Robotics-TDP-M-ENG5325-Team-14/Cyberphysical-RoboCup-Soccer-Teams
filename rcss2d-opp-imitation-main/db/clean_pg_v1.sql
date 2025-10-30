7ef08b35eb61b6830d38124f36e048ae2367d413
Nathan <69724075+Nabakator@users.noreply.github.com>
2025-10-30 03:15:41 +0000

    Delete rcss2d-opp-imitation-main directory

--- rcss2d-opp-imitation-main/db/clean_pg_v1.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM extra.matchstates WHERE playmode != 'play_on';
-
-DELETE FROM extra.playercommands WHERE teamname_fk != 'HELIOS2019';
-
-DELETE FROM extra.playercommands WHERE unum_fk = 1;
